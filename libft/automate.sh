8e01656fe810a9ac10ce84b19a000e4d242e4ed2
Bernardo <bercbranco@gmail.com>
2022-11-07 12:23:22 +0000

    Correcting an unwanted behavour

--- libft/automate.sh
+++ libft/automate.sh
@@ -3,9 +3,9 @@
 # Change path to libft
 PATH_LIBFT=~/Desenvolvimento/42/projects/Libft
 
+make fclean
 make -C "${PATH_LIBFT}" fclean bonus
 cp "${PATH_LIBFT}"/libft.a libft.a
 cp "${PATH_LIBFT}"/libft.h libft.h
-make fclean
 make all
 ./testing
