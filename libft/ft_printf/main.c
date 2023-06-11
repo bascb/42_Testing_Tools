e1483393231bb796db80634b4933834e767042ae
Bernardo <bercbranco@gmail.com>
2023-06-11 18:23:28 +0000

    Adding a littleft_printf test to libft

---
+++ libft/ft_printf/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "libft.h"
+
+int	main(int argc, char **argv)
+{
+	int	test;
+
+	if (argc < 2)
+		return (0);
+	test = atoi(argv[1]);
+	if (test == 1)
+		printf("abcdefghijklmnopqrstuvwxyz");
+	if (test == 2)
+		printf("abcdefghijklmnokkpqrstuvwxyz");
+	if (test == 3)
+		printf("abcdefghijklmnok\n\tpqrstuvwxyz");
+	return (0);
+}
---
+++ libft/ft_printf/main_ft.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "libft.h"
+
+int	main(int argc, char **argv)
+{
+	int	test;
+
+	if (argc < 2)
+		return (0);
+	test = atoi(argv[1]);
+	if (test == 1)
+		ft_printf("abcdefghijklmnopqrstuvwxyz");
+	if (test == 2)
+		ft_printf("abcdefghijklmnokkpqrstuvwxyz");
+	if (test == 3)
+		ft_printf("abcdefghijklmnok\n\tpqrstuvwxyz");
+	return (0);
+}
