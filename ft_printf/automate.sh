d70c1eee4a1b36759acf0926ffce84402cad8244
Bernardo <bercbranco@gmail.com>
2023-06-11 17:36:26 +0000

    Changing libftprintf.h to ft_printf.h

--- ft_printf/automate.sh
+++ ft_printf/automate.sh
@@ -5,7 +5,7 @@ PATH_PRINTF=~/Desenvolvimento/42/projects/ft_printf
 
 make -C "${PATH_PRINTF}" re
 cp "${PATH_PRINTF}"/libftprintf.a libftprintf.a
-cp "${PATH_PRINTF}"/libftprintf.h libftprintf.h
+cp "${PATH_PRINTF}"/ft_printf.h ft_printf.h
 cp "${PATH_PRINTF}"/libft.h libft.h
 make fclean
 make all
